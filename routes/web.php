656af19c999a27a650587dea8a89b4d94d7f445f
dimany37 <dimany37@mail.ru>
2020-06-01 21:49:59 +0400

    wwwfff

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,24 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('rest','RestTestController');
+//Route::resource('rest','RestTestController');
 //->names('restTest');
 Route::group(['namespace' => 'Blog', 'prefix' => 'blog'], function() {
     Route::resource('posts', 'PostController')->names('blog.posts');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+//админка блога
+$groupData = [
+    'namespace' => 'Blog\Admin',
+    'prefix'    => 'admin/blog',
+];
+Route::group($groupData, function() {
+    $methods = ['index', 'edit', 'store', 'update', 'create',];
+    Route:: resource('categories', 'CategoryController')
+        ->only($methods)->names('blog.admin.categories');
 });
